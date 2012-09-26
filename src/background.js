d917d38fe5808c9ce012884dce3e59ec6aa5ff41
chitsaou <chitsaou@techbang.com.tw>
2012-09-26 18:35:35 +0800

    this plugin blocks Facebook social plugins only

--- src/background.js
+++ src/background.js
@@ -1,17 +1,11 @@
 var listener = function(details) {
-  if (details.url.indexOf("blog.chinatimes.com") !== -1) {
-    return {};
-  } else {
-    return {
-      cancel: true
-    };
-  }
+  return {
+    cancel: true
+  };
 };
 
 var urls = [
-  "*://*.chinatimes.com/*",
-  "*://*.ctitv.com.tw/*",
-  "*://*.ctv.com.tw/*"
+  "https://www.facebook.com/plugins/*"
 ];
 
 chrome.webRequest.onBeforeRequest.addListener(listener, { urls: urls }, [ "blocking" ]);
